51abe204605a0c1ddcc995aacac7f272fc9743af
anisiabarros <anisia.barros@gmail.com>
2024-07-07 12:02:54 -0300

    commit inicial excluindo um folder problematico

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CabecalhoComponent } from './componente/cabecalho/cabecalho.component';
+import { RodapeComponent } from './componente/rodape/rodape.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CabecalhoComponent,
+    RodapeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
